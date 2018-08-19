b34adef0d480ac2d7d1628d55d07802f4f0925bc
Anatoly <AnatoliySokolov1981@gmail.com>
2018-08-19 23:42:27 +0300

    Генерация строк таблицы bids из тестового массива

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,35 @@
 'use strict';
 
+// var data = {
+//   "name": "BTC/USD",
+//   "asks": [{
+//     "numberOfOrders": 1,
+//     "price": 6074.9,
+//     "quantity": 3.93318116
+//   }, {
+//     "numberOfOrders": 1,
+//     "price": 6080,
+//     "quantity": 0.01
+//   }, {
+//     "numberOfOrders": 1,
+//     "price": 6081.04,
+//     "quantity": 34.6
+//   }],
+//   "bids": [{
+//     "numberOfOrders": 3,
+//     "price": 6074.6,
+//     "quantity": 4.86156108
+//   }, {
+//     "numberOfOrders": 9,
+//     "price": 6074.5,
+//     "quantity": 15.7519723
+//   }, {
+//     "numberOfOrders": 17,
+//     "price": 6074.46,
+//     "quantity": 1.19
+//   }]
+// }
+
 var data = {
   "name": "BTC/USD",
   "asks": [{
@@ -14,25 +44,12 @@ var data = {
     "numberOfOrders": 1,
     "price": 6081.04,
     "quantity": 34.6
-  }],
-  "bids": [{
-    "numberOfOrders": 3,
-    "price": 6074.6,
-    "quantity": 4.86156108
-  }, {
-    "numberOfOrders": 9,
-    "price": 6074.5,
-    "quantity": 15.7519723
-  }, {
-    "numberOfOrders": 17,
-    "price": 6074.46,
-    "quantity": 1.19
   }]
 }
 
 var bidsTable = document.querySelector('.order__tbody--bids'),
   asksTable = document.querySelector('.order__tbody--asks'),
-  rowTemplate = document.querySelector('.row-template').content.querySelector('.template__tr');
+  rowTemplate = document.querySelector('.row-template').content.querySelector('.order__tr');
 
 var renderRow = function (obj) {
   var rowElement = rowTemplate.cloneNode(true);
@@ -41,10 +58,17 @@ var renderRow = function (obj) {
     amount = rowElement.querySelector('.order__td--thrid-column'),
     price = rowElement.querySelector('.order__td--fourth-column');
 
-  count.textContent = data.asks.numberOfOrders;
-  total.textContent = data.asks.quantity;
-  amount.textContent = data.asks.quantity;
-  price.textContent = data.asks.price;
+  count.textContent = obj.numberOfOrders;
+  total.textContent = 'сумма';
+  amount.textContent = obj.quantity;
+  price.textContent = obj.price;
 
   return rowElement;
 };
+
+var fragment = document.createDocumentFragment();
+Array.from(data.asks).forEach(function (el) {
+  fragment.appendChild(renderRow(el));
+})
+
+bidsTable.appendChild(fragment);
